fa9d5419039107b9e55ce91e50f5a1163a5283b9
Pascal Schärli <github@pascscha.ch>
2023-03-13 10:36:57 +0100

    Formatting, pep8 compliance

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import json
 from telegram import Update
-from telegram.ext import ApplicationBuilder, filters, ContextTypes, MessageHandler, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    filters,
+    ContextTypes,
+    MessageHandler,
+    CommandHandler,
+)
 
 from datetime import date
 import random
@@ -24,14 +30,14 @@ async def results(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     scores = []
     votes = []
 
-    for i in range(0,len(lines)):
+    for i in range(0, len(lines)):
         if "results" in lines[i]:
             continue
         if ":" in lines[i]:
             if "+" in lines[i]:
                 mensa = lines[i].split(":")[0]
                 score = int(lines[i].split("+")[1])
-                vote = {"mensa":mensa, "score":score}
+                vote = {"mensa": mensa, "score": score}
                 votes.append(vote)
                 scores.append(score)
 
@@ -39,33 +45,30 @@ async def results(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         max_score = max(scores)
     else:
         return await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="No mensas/scores found in message"
+            chat_id=update.effective_chat.id, text="No mensas/scores found in message"
         )
 
-    mensas=[]
+    mensas = []
     for vote in votes:
         if vote["score"] == max_score:
             mensas.append(vote["mensa"])
-
 
     mensas.sort()
     reply = "Mensas under consideration:\n"
     for mensa in mensas:
         reply += "- " + mensa + "\n"
-    reply+="The winner is: " + random.choice(mensas)
-    reply+="\nThe seed was: "+ today_str
+    reply += "The winner is: " + random.choice(mensas)
+    reply += "\nThe seed was: " + today_str
 
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=reply
-    )
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Forward me a Decidobot3000 tied results message"
+        text="Forward me a Decidobot3000 tied results message",
     )
+
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.ALL, results))
